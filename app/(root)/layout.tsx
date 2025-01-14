d87aa4809f5f52f9657560fae694596489b6138d
Emilia <81705094+isemilia@users.noreply.github.com>
2025-01-14 12:01:34 +0500

    Install prettier

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-import React, {ReactNode} from "react";
-import Navbar from "@/app/components/navbar";
+import React, { ReactNode } from 'react'
+import Navbar from '@/app/components/navbar'
 
-export default function Layout({children}: { children: ReactNode }) {
-    return (
-        <main className={'font-work-sans'}>
-            <Navbar/>
-            
-            {children}
-        </main>
-    )
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <main className={'font-work-sans'}>
+      <Navbar />
+
+      {children}
+    </main>
+  )
 }
