b9f192e0754e2a5a886f4658107c90ca298fe120
Emilia <81705094+isemilia@users.noreply.github.com>
2025-01-14 11:31:51 +0500

    Sign in & sign out with github

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,54 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import {auth, signOut, signIn} from "@/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const session = await auth()
+
+    // console.log(session)
+
     return (
         <header className={'px-5 py-3 bg-white shadow-sm'}>
             <nav className="flex justify-between items-center">
                 <Link href={'/'}>
                     <Image src={'/logo.png'} alt={'logo'} width={144} height={30}/>
                 </Link>
-                <div className="flex items-center gap-5"></div>
+                <div className="flex items-center gap-5">
+                    {session && session.user ? (
+                        <>
+                            <Link href={'/startup/create'}>
+                                <span>Create</span>
+                            </Link>
+
+                            <form action={async () => {
+                                'use server';
+
+                                await signOut({redirectTo: '/'})
+                            }}>
+                                <button type="submit">
+                                    <span>Sign out</span>
+                                </button>
+                            </form>
+
+                            <Link href={`/user/${session.user.id}`}>
+                                <span>{session.user.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <form action={async () => {
+                                'use server';
+
+                                await signIn('github')
+                            }}>
+                                <button type="submit">
+                                    <span>Sign in</span>
+                                </button>
+                            </form>
+                        </>
+                    )}
+                </div>
             </nav>
         </header>
     )
